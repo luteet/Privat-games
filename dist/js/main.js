7e4bcbd86003ae932c6f0a0b13d91b7fa674385a
Roman Lukjanets <64837849+luteet@users.noreply.github.com>
2022-09-21 17:20:23 +0300

    💀

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,17 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+  testWebP(document.body)
+})
+
+function testWebP(elem) {
+  const webP = new Image();
+  webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+  webP.onload = webP.onerror = function () {
+    webP.height === 2 ? elem.classList.add('webp-true') : elem.classList.add('webp-false')
+  }
+  //console.log(webP)
+}
+
 let slideCheck = true;
 let slideUp = (target, duration=500) => {
   slideCheck = false;
